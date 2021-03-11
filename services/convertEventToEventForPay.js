534a0b8be491f465cd7ef1099d688922007ac890
gurvan <gurvan.t@orange.fr>
2021-03-11 10:10:54 +0100

    implémentation récursivité (KO)

--- services/convertEventToEventForPay.js
+++ services/convertEventToEventForPay.js
@@ -1,8 +1,8 @@
 const { Event } = require("../objects/event")
 const { EventForPay } = require("../objects/eventForPay")
 
-const HOUR_START_DAY = 6
-const HOUR_END_DAY = 22
+const NIGHT_TIME_START = 22
+const NIGHT_TIME_END = 7
 
 /**
  * @function
@@ -10,58 +10,85 @@ const HOUR_END_DAY = 22
  * @returns {EventForPay[]}
  */
 module.exports.convertEventToEventForPay = function (event) {
-    const { startDate, endDate } = event
+    /**
+     * @param {Date} date
+     * @returns {boolean}
+     */
+    const nightTime = date => date >= nightTimeStartForDate(date) && date < nightTimeEndForDate(date)
 
-    /** Hours difference between the two dates */
-    const hoursDiff = Math.abs(startDate.getTime() - endDate.getTime()) / 36e5
+    /**
+     * @param {Date} date
+     * @returns {boolean}
+     */
+    const dayTime = date => !nightTime(date)
 
-    /** List of hours betweent the two dates */
-    const hours = new Array(hoursDiff)
-        .fill()
-        .map((_, i) => ({
-            sum: i + startDate.getHours(),
-            clean: (i + startDate.getHours()) % 24
-        }))
+    /**
+     * @param {Date} date
+     * @returns {Date}
+     */
+    const nightTimeStartForDate = date => new Date(date.getFullYear(), date.getMonth(), date.getDate(), NIGHT_TIME_START)
 
-    /** @type {Date[][]} Array of array of date with bounded interval */
-    const superDates = hours.reduce((acc, hour, index) => {
-        /** Date to be pushed */
-        const dt = new Date(
-            startDate.getFullYear(),
-            startDate.getMonth(),
-            startDate.getDate(),
-            hour.sum
-        )
+    /**
+     * @param {Date} date
+     * @returns {Date}
+     */
+    const nightTimeEndForDate = date => {
+        const day = (() => {
+            if (date.getHours() > NIGHT_TIME_END)
+                return date.getDate() + 1
+            else
+                return date.getDate()
+        })()
+        return new Date(date.getFullYear(), date.getMonth(), day, NIGHT_TIME_END)
+    }
 
-        //If there is a value AND hour it's not a terminal
-        if (
-            !!acc[acc.length - 1] &&
-            ![HOUR_START_DAY, HOUR_END_DAY].includes(hour.clean)
-        ) {
-            //If just before terminal OR last of array
-            if (
-                [HOUR_START_DAY - 1, HOUR_END_DAY - 1].includes(hour.clean) ||
-                hours.length - 1 === index
-            ) {
-                dt.setMinutes(59)
-                dt.setSeconds(59)
-            }
+    /**
+     * @param {Date} date
+     * @param {Date} duration
+     * @returns {number[]}
+     */
+    const shouldSplitCalculation = (date, duration) => {
+        // const totalDuration = (duration.getTime() - date.getTime()) / 1000
+        const totalDuration = duration.getSeconds() + (duration.getMinutes() * 60) + (duration.getHours() * 3600)
+        const untilSwitch = (() => {
+            if (dayTime(date))
+                return (nightTimeStartForDate(date).getTime() - date.getTime()) / 1000
+            else
+                return (nightTimeEndForDate(date).getTime() - date.getTime()) / 1000
+        })()
 
-            //Push dt in current array
-            acc[acc.length - 1].push(dt)
+        return [totalDuration, untilSwitch]
+    }
+
+    /**
+     * @param {Date} date
+     * @param {Date} duration
+     * @param {number} metierId
+     */
+    const splitEventForPay = (date, duration, metierId) => {
+        const [totalDuration, untilSwitch] = shouldSplitCalculation(date, duration)
+        if (untilSwitch > totalDuration) {
+            return [new EventForPay({
+                startDate: date,
+                endDate: duration
+            })]
         } else {
-            //Create new array with dt
-            acc.push([dt])
+            const firstDuree = new Date(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), date.getMinutes(), untilSwitch)
+
+            const nextDuree = new Date(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), date.getMinutes(), totalDuration - untilSwitch)
+
+            const nextDate = new Date(date.getTime())
+            nextDate.setSeconds(nextDate.getSeconds() + untilSwitch)
+
+            return [
+                new EventForPay({
+                    startDate: date,
+                    endDate: firstDuree
+                }),
+                ...splitEventForPay(nextDate, nextDuree, metierId)
+            ]
         }
+    }
 
-        return acc
-    }, [])
-
-    return superDates
-        .map(dates => new EventForPay({
-            startDate: dates[0],
-            endDate: dates[dates.length - 1],
-            hourWorkDay: dates.filter(x => x.getHours() >= HOUR_START_DAY && x.getHours() < HOUR_END_DAY).length,
-            hourWorkNight: dates.filter(x => x.getHours() < HOUR_START_DAY || x.getHours() >= HOUR_END_DAY).length
-        }))
+    return splitEventForPay(event.startDate, event.endDate, 1)
 }
